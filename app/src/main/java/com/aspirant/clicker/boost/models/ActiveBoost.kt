cb154cb0af978ddae17183b372cc53757e377a4f
AspirantDrago <sanekclubstr@mail.ru>
2025-02-10 15:02:06 +0500

    Add Boost Fragment

---
+++ app/src/main/java/com/aspirant/clicker/boost/models/ActiveBoost.kt
@@ -0,0 +1,8 @@
+package com.aspirant.clicker.boost.models
+import com.aspirant.clicker.boost.AbsBoost
+
+class ActiveBoost: AbsBoost() {
+    override fun buy() {
+        TODO("Not yet implemented")
+    }
+}
