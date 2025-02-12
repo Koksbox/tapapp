3d59bfb312d1195a39db88d7839d064b8648c5ea
AspirantDrago <sanekclubstr@mail.ru>
2025-02-12 15:00:20 +0500

    Add Boosts ScrollView

--- app/src/main/java/com/aspirant/clicker/boost/BoostView.kt
+++ app/src/main/java/com/aspirant/clicker/boost/BoostView.kt
@@ -7,20 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.aspirant.clicker.R
 
-// TODO: Rename parameter arguments, choose names that match
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM_TITLE = "title"
 
 class BoostView : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
+    private var title: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
+            title = it.getString(ARG_PARAM_TITLE)
         }
     }
 
@@ -28,17 +23,15 @@ class BoostView : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_boost_view, container, false)
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(title: String) =
             BoostView().apply {
                 arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM_TITLE, title)
                 }
             }
     }
